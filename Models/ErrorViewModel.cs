1f68e819766256492686c0a8167b49c34f054d2c
Juliano Candido <julianosgs@yahoo.com.br>
2024-05-06 13:14:02 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Impacto_Solucoes_Digitais.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
