b4d80897f4e5022efd7046c359c2bc274f7670cb
Juliano Candido <julianosgs@yahoo.com.br>
2024-06-28 12:55:43 -0300

    Conclusão ft_Pagina_Index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,21 @@
-using Impacto_Solucoes_Digitais.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace Impacto_Solucoes_Digitais.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult SobreNos()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
     }
 }
